98eeeeb8623489b78d649541e84a830c89c01ab3
Will Acton <will@willacton.com>
2023-12-03 11:43:02 -0800

    prettier on ts

--- src/day3.ts
+++ src/day3.ts
@@ -1,5 +1,4 @@
-let example =
-  `467..114..
+let example = `467..114..
 ...*......
 ..35..633.
 ......#...
@@ -10,107 +9,103 @@ let example =
 ...$.*....
 .664.598..`;
 
-
 type SchematicValue = {
-  value: string,
-  y: number,
-  start: number,
-  end: number,
-}
+  value: string;
+  y: number;
+  start: number;
+  end: number;
+};
 
 type Schematic = {
-  parts: SchematicValue[],
-  symbols: SchematicValue[]
-}
+  parts: SchematicValue[];
+  symbols: SchematicValue[];
+};
 
-
-function matchSchematicValues(lines: string[], regex: RegExp): SchematicValue[] {
+function matchSchematicValues(
+  lines: string[],
+  regex: RegExp,
+): SchematicValue[] {
   let values = lines.flatMap((line, y) => {
     let matches = line.matchAll(regex);
-    let values = []
+    let values = [];
     for (let match of matches) {
       if (match.index === undefined) {
-        throw new Error("this is impossible. stfu typescript")
+        throw new Error("this is impossible. stfu typescript");
       }
       values.push({
         value: match[0],
         y,
         start: match.index,
         end: match.index + match[0].length - 1,
-      })
+      });
     }
     return values;
   });
   return values;
 }
 
-
 function parseSchematic(input: string): Schematic {
-  let lines = input.split('\n');
-  let parts = matchSchematicValues(lines, /(\d)+/g)
+  let lines = input.split("\n");
+  let parts = matchSchematicValues(lines, /(\d)+/g);
   let symbols = matchSchematicValues(lines, /([^\d^\.])+/g);
   return { parts, symbols };
 }
 
-parseSchematic(example)
-
+parseSchematic(example);
 
 function isAdjacent(a: SchematicValue, b: SchematicValue): boolean {
-  if (Math.abs(a.y - b.y) > 1) return false
-  if (a.start - b.start > 1) return false
-  if (a.end - b.end < -1) return false
-  return true
+  if (Math.abs(a.y - b.y) > 1) return false;
+  if (a.start - b.start > 1) return false;
+  if (a.end - b.end < -1) return false;
+  return true;
 }
 
 isAdjacent(
-  { value: "123", y: 0, start: 0, end: 2},
-  { value: "*", y: 1, start: 3, end: 3}
-) // true
+  { value: "123", y: 0, start: 0, end: 2 },
+  { value: "*", y: 1, start: 3, end: 3 },
+); // true
 
 isAdjacent(
-  { value: "123", y: 0, start: 0, end: 2},
-  { value: "*", y: 2, start: 3, end: 3}
-) // false
+  { value: "123", y: 0, start: 0, end: 2 },
+  { value: "*", y: 2, start: 3, end: 3 },
+); // false
 
 isAdjacent(
-  { value: "123", y: 0, start: 0, end: 2},
-  { value: "*", y: 1, start: 4, end: 4}
-) // false
+  { value: "123", y: 0, start: 0, end: 2 },
+  { value: "*", y: 1, start: 4, end: 4 },
+); // false
 
 isAdjacent(
-  { value: "123", y: 0, start: 0, end: 2},
-  { value: "*", y: -1, start: 3, end: 3}
-) // true
-
+  { value: "123", y: 0, start: 0, end: 2 },
+  { value: "*", y: -1, start: 3, end: 3 },
+); // true
 
 function part1({ parts, symbols }: Schematic): number {
   return parts
-    .filter(part => symbols.some(symbol => isAdjacent(part, symbol)))
+    .filter((part) => symbols.some((symbol) => isAdjacent(part, symbol)))
     .reduce((sum, part) => sum + parseInt(part.value), 0);
 }
 
 part1(parseSchematic(example)); // 4361
 
-let fs = require('node:fs');
+let fs = require("node:fs");
 
-part1(parseSchematic(fs.readFileSync('../inputs/day3', 'utf8')));
-
+part1(parseSchematic(fs.readFileSync("../inputs/day3", "utf8")));
 
 function part2({ parts, symbols }: Schematic): number {
   let gears = 0;
   for (let symbol of symbols) {
-    if (symbol.value === '*') {
-      let adjacent = parts.filter(part => isAdjacent(part, symbol));
+    if (symbol.value === "*") {
+      let adjacent = parts.filter((part) => isAdjacent(part, symbol));
       if (adjacent.length === 2) {
         let [a, b] = adjacent;
         gears += parseInt(a.value) * parseInt(b.value);
       }
     }
   }
-  return gears
+  return gears;
 }
 
 part2(parseSchematic(example)); // 467835
 
-
-part2(parseSchematic(fs.readFileSync('../inputs/day3', 'utf8')));
+part2(parseSchematic(fs.readFileSync("../inputs/day3", "utf8")));
